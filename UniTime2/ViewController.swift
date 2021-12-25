239e826fe3bf417b1fd4b3f2fcf254a5bb41f411
dazy1030 <dev.nodajima@gmail.com>
2021-12-26 00:04:56 +0900

    Initial Commit

---
+++ UniTime2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UniTime2
+//
+//  Created by Naoki Odajima on 2021/12/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
