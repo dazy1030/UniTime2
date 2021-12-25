04bee9af9351a8c04ccd7ba368e255678295283e
dazy1030 <dev.nodajima@gmail.com>
2021-12-26 01:59:44 +0900

    fix: final付与

--- UniTime2/AppDelegate.swift
+++ UniTime2/AppDelegate.swift
@@ -8,7 +8,7 @@
 import Cocoa
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
